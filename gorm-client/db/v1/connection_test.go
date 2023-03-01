de0f7b332b28b70041901d7a7932a7b8d347a58a
Raja Soundaramourty <rajasoun@icloud.com>
2023-02-28 19:34:58 -0500

    v1 db connection mgmt

--- gorm-client/db/v1/connection_test.go
+++ gorm-client/db/v1/connection_test.go
@@ -8,7 +8,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewDB(t *testing.T) {
+func TestNewDBWithDockerTest(t *testing.T) {
+	pool, resource := test.InitTestDocker("3306")
+	// stop container after test
+	defer func() {
+		if err := pool.Purge(resource); err != nil {
+			log.Println("Failed to purge resource")
+		}
+		log.Println("Purged resource")
+	}()
+
+	dbConn, err := NewDB()
+
+	assert.NoError(t, err, "Failed to connect to database")
+	assert.NotNil(t, dbConn, "Database connection is nil")
+}
+
+func TestNewDBWithInMemoryDB(t *testing.T) {
 	pool, resource := test.InitTestDocker("3306")
 	// stop container after test
 	defer func() {
